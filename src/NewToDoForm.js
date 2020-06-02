e7baf60bd494099052700568b1ffdfdf9341d29c
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-02 14:00:49 -0700

    finish form component

--- src/NewToDoForm.js
+++ src/NewToDoForm.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
 
 class NewToDoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+  }
+
   render() {
-    return;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="item">New Todo</label>
+        <input
+          id="item"
+          name="item"
+          value={this.state.item}
+          onChange={this.handleChange}
+        />
+        <button type="submit">ADD TODO</button>
+      </form>
+    );
   }
 }
 
